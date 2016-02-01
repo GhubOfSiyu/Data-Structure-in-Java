e2326b4eab5f79b49c363e3bf360dc3d1e602dc6
Siyu Chen <schen@localhost.localdomain>
2016-02-01 16:48:52 -0500

    Stack Queue ArrayList

---
+++ Fundamental_Data_Structures/Stack.java
@@ -0,0 +1,10 @@
+package Fundamental_Data_Structure;
+import java.util.*;
+class Stack<E> {
+	private LinkedList<E> head = new LinkedList<E>();
+	public boolean isEmpty() {return head.size() == 0;}
+	public int size() {return head.size();}
+	public void push(E e) {head.addFirst(e);}
+	public E pop() {return head.removeFirst();}
+	public E top() {return head.getFirst();}
+}
